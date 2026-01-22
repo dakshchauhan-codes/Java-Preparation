c4c4f4384eab140be64ec6c6cc930beff761e788
Daksh Chauhan <dakshh1120@gmail.com>
2026-01-22 11:15:29 +0530

    encapsulation

--- Demo.java
+++ Demo.java
@@ -1,6 +1,16 @@
- public class Demo
-{
-    public static void main (String args []) {
-        System.out.println("hello java");
+public class Demo {
+    int a;
+     int b;
+       void getData() {
+            a = 10;
+            b = 20;
+        }
+        void showData() {
+            System.out.println("a :"+a + " "+ "b: "+b);
+        }
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        obj.getData();
+      obj.showData();
     }
 }
